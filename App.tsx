cc519c69fc91b23d6e93320b48ee1ba33736dd8e
akash <akash@codiis.com>
2024-01-11 17:14:04 +0530

    changes in kt file

--- App.tsx
+++ App.tsx
@@ -8,9 +8,13 @@
 import React from 'react';
 // import My_Bottom_Tab from './navigation/My_Bottom_Tab';
 import My_stack from './navigation/My_Stack';
+import AudioList from './pages/AudioList';
 
 const App = () => {
-  return <My_stack />;
+  return (
+    // <My_stack />
+    <AudioList />
+  );
 };
 
 export default App;
---
+++ pages/AudioList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+
+function AudioList() {
+
+  return (
+    <View>
+      <Text>AudioList</Text>
+    </View>
+  )
+}
+
+export default AudioList
